d294a90f54ef74fdc12a4a54d395c0c5d17e89f6
Joe <joseph.fabel@gmail.com>
2023-10-19 22:18:00 -0700

    Updated checkings and savings account

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,3 +1,14 @@
+package ATM;
 public class CheckingAccount extends Account {
-    private double balanceChecking;
+    private double checkingBalance;
+
+    // checkingBalance getter
+    public double getCheckingBalance() {
+        return checkingBalance;
+    }
+
+    // checkingBalance setter
+    public void setCheckingBalance(double balance) {
+        checkingBalance = balance;
+    }
 }
--- CreditAccount.java
+++ CreditAccount.java
@@ -1,4 +1,25 @@
+package ATM;
 public class CreditAccount extends Account {
     private double creditLimit;
     private double creditUsed;
+
+    // creditLimit getter
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    // creditLimit setter
+    public void setCreditLimit(double limit) {
+        creditLimit = limit;
+    }
+
+    // creditUsed getter
+    public double getCreditUsed() {
+        return creditUsed;
+    }
+
+    // creditUsed setter
+    public void setCreditUsed(double credit) {
+        creditUsed = credit;
+    }
 }
