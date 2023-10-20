b69bf6d096c40b0384256145fd68acd2ca5f3948
Joe <joseph.fabel@gmail.com>
2023-10-19 22:22:08 -0700

    Finished Card

--- Card.java
+++ Card.java
@@ -4,11 +4,33 @@ public abstract class Card {
     protected String expirationDate;
     protected int ccvNumber;
 
+    // cardNumber getter
     public int getCardNumber() {
         return cardNumber;
     }
 
+    // cardNumber setter
     public void setCardNumber(int number) {
         cardNumber = number;
+    }
+
+    // expirationDate getter
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    // expirationDate setter
+    public void setExpirationDate(String date) {
+        expirationDate = date;
+    }
+
+    // ccvNumber getter
+    public int getCCVNumber() {
+        return ccvNumber;
+    }
+
+    // ccvNumber setter
+    public void setCCVNumber(int number) {
+        ccvNumber = number;
     }
 }
