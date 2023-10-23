0fb3cc410aa4d642aa1c8d57e12f26746979a632
KaydenDaKat <48571593+KaydenDaKat@users.noreply.github.com>
2023-10-23 10:25:13 -0700

    Update SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -30,4 +30,11 @@ public class SavingsAccount extends Account {
     public void setInterestRate(double rate) {
         interestRate = rate;
     }
+
+    // Transfer money to another account
+    // Take in the amount of money that is requested to be transfered,
+    //    and take in the accountNumber that the money is being transferred to
+    // Return true if the transfer is successful, false if not.
+    // Method should ensure transferAmount is within the balance the sender account
+    public boolean transferFunds(double transferAmount, int accountNumber)
 }
