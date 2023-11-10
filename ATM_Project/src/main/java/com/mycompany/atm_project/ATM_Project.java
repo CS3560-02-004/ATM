d323d739f5e15873ea09e056b36eeb23e2f817b7
KaydenDaKat <48571593+KaydenDaKat@users.noreply.github.com>
2023-11-09 19:06:32 -0800

    Added Netbeans project

---
+++ ATM_Project/src/main/java/com/mycompany/atm_project/ATM_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atm_project;
+
+/**
+ *
+ * @author gippy
+ */
+public class ATM_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
