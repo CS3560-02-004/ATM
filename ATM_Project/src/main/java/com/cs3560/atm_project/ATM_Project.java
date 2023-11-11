4cf839dbe3bedb4eccbc7e37011555905b746258
KaydenDaKat <48571593+KaydenDaKat@users.noreply.github.com>
2023-11-10 19:58:39 -0800

    Redo ATM_Project folder
    Added a screen for the enter card

---
+++ ATM_Project/src/main/java/com/cs3560/atm_project/ATM_Project.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cs3560.atm_project;
+
+/**
+ *
+ * @author gippy
+ */
+public class ATM_Project {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+        Login_GUI loginGui = new Login_GUI();
+        loginGui.run();
+    }
+}
